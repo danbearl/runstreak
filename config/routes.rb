ec16ad326aee73e8ffeec39e8b31c551a3571d16
Dan Bearl <dan@sleepybadger.com>
2015-10-04 20:43:49 -0400

    Fixing layout issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  get '/log_out' => 'sessions#destroy', as: 'log_out'
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :runs, only: [:new, :create]
