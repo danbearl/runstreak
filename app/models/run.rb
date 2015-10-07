778de929012a3f3a88b6e3289fee774e9783778e
Dan Bearl <dan@sleepybadger.com>
2015-10-07 07:39:26 -0400

    Fixing display format and distance validation

--- app/models/run.rb
+++ app/models/run.rb
@@ -2,5 +2,5 @@ class Run < ActiveRecord::Base
   belongs_to :user
 
   validates :ran_on, presence: true
-  validates :distance, numericality: true
+  validates :distance, numericality: { greater_than_or_equal_to: 1 }
 end
